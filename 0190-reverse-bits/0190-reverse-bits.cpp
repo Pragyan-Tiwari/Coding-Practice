f053080eb64b539d10abe0ac2f86510493086538
Pragyan Tiwari <pragyan.0111it231077@technocratsgroup.edu.in>
2026-01-08 19:57:31 +0530

    Time: 0 ms (100%), Space: 8.3 MB (43.76%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        if(n==0)return 0;
+        uint32_t rev=0;
+        for(int i=0;i<32;i++){
+            rev<<=1;
+            rev|=(n&1);
+            n>>=1;
+        }
+        return rev;
+    }
+};
