db575723ed2fa2687dbbec1f857e4ee25c0e1017
Pragyan Tiwari <pragyan.0111it231077@technocratsgroup.edu.in>
2026-01-08 19:55:59 +0530

    Time: 0 ms (100%), Space: 97.4 MB (59.86%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int min=INT_MAX;
+        int maxprofit=0;
+        for(int i=0;i<prices.size();i++){
+            if(prices[i]<min)
+        min=prices[i];
+        else{
+            maxprofit=max(maxprofit,prices[i]-min);
+        }
+        }
+        return maxprofit;
+    }
+};
