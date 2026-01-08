8c8e1c9c997318e8043f69122c5020759bad3cdd
Pragyan Tiwari <pragyan.0111it231077@technocratsgroup.edu.in>
2026-01-08 19:48:27 +0530

    Time: 0 ms (100%), Space: 14.8 MB (53.14%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int>hash;
+        for(int i=0;i<nums.size();i++){
+            int rem=target-nums[i];
+            if(hash.find(rem)!=hash.end()){
+                return {hash[rem],i};
+            }
+            hash[nums[i]]=i;
+        }
+        return{-1,-1};
+    }
+};
