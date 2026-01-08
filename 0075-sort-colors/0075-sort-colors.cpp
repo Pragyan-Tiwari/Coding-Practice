3100028b45a4fc5f31841222e2c95bc9613f684e
Pragyan Tiwari <pragyan.0111it231077@technocratsgroup.edu.in>
2026-01-08 19:55:47 +0530

    Time: 0 ms (100%), Space: 11.7 MB (46.93%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int i=0,mid=0,j=nums.size()-1;
+        while(mid<=j){
+            if(nums[mid]==0){
+                swap(nums[i],nums[mid]);
+                i++;mid++;
+            }
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else{
+                swap(nums[mid],nums[j]);
+                j--;
+            }
+
+
+
+        }
+    }
+};
